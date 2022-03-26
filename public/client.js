e0efa38e37f26dbcc77835a1f873d98567354613
Tân Vũ <vungoctan9x@gmail.com>
2022-03-27 00:27:08 +0700

    match/games

--- public/client.js
+++ public/client.js
@@ -50,12 +50,14 @@ socket.on('game-data', (data, callback) => {
 	game_state.game.opp.score = data.opp_score;
 	game_state.game.ball = data.ball;
 	game_state.game.opp.pos = data.opp_pos;
+	game_state.game.self.winGames = data.self_win;
+	game_state.game.opp.winGames = data.opp_win;
 	callback(game_state.game.self.pos);
 
 	if (data.game_state == 2) {
 		setTimeout(() => {
 			document.getElementById('gameplay').style.display = 'none';
-			alert(`Player ${data.winner} win!`)
+			console.log(`Player ${data.winner} win!`)
 		}, 3000);
 	}
 });
