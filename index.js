ddaf08c54c697f9db73dc773483e590ca4490281
Tân Vũ <vungoctan9x@gmail.com>
2022-03-26 22:11:17 +0700

    init commit

---
+++ index.js
@@ -0,0 +1,202 @@
+var express = require('express');
+var Game = require('./Game');
+var app = express();
+const HERTZ = 30; //Game updates per second
+const port = process.env.PORT || 80;
+var server = require('http')
+	.createServer(app)
+	.listen(port);
+var io = require('socket.io')(server);
+const uNRegex = new RegExp('^[a-zA-Z0-9_.-]{3,}$');
+
+app.use(express.static(__dirname + '/node_modules'));
+app.use(express.static(__dirname + '/public'));
+app.get('/', function (req, res, next) {
+	res.sendFile(__dirname + '/public/index.html');
+});
+
+//User class
+class User {
+	constructor(socket) {
+		this.socket = socket;
+		this.username = socket.id;
+		this.game = { id: null, playing: false };
+	}
+}
+
+//Stores all connected users
+let users = {};
+let matchmaking = [];
+let games = {};
+
+//Manages sockets
+io.on('connection', socket => {
+	console.log(`New client connected: ${socket.id}`);
+	users[socket.id] = new User(socket);
+	socket.broadcast.emit('player-broadcast', Object.keys(users).length);
+	socket.emit('player-broadcast', Object.keys(users).length);
+	//Checks for duplicate usernames
+	socket.on('set-username', (username, callback) => {
+		let same_username = false;
+		// console.log(uNRegex.test(username));
+		if (!uNRegex.test(username)) {
+			callback(false);
+			same_username = true;
+		}
+		for (var key in users) {
+			var user = users[key];
+			if (user.username == username) {
+				callback(false);
+				same_username = true;
+			}
+		}
+		if (!same_username && users[socket.id].username == socket.id) {
+			console.log(
+				`${users[socket.id].username} set their username to ${username}`
+			);
+			users[socket.id].username = username;
+			callback(true);
+			socket.emit('matchmaking-begin');
+			matchMaker(socket.id);
+		} else {
+			console.log(
+				`${users[socket.id].username} tried to set their username to ${username}, however username is in use or invalid.`
+			);
+		}
+	});
+
+	socket.on('get-ping', callback => {
+		callback(true);
+	});
+
+	socket.on('search-users', (data) => {
+		const result = [];
+		for (var key in users) {
+			var user = { id: key, username: users[key].username };
+			console.log('search-xxxx', user);
+			if (user.username.trim().toLowerCase().includes(data.trim().toLowerCase())) {
+				result.push(user);
+			}
+		}
+		socket.emit('search-users-result', result);
+		console.log('search-users', data, users, result);
+	});
+
+	//Disconnects user
+	socket.on('disconnect', () => {
+		console.log(`Client Disconnected: ${users[socket.id].username}`);
+		delete users[socket.id];
+		socket.broadcast.emit('player-broadcast', Object.keys(users).length);
+		if (matchmaking.length != 0 && matchmaking[0] == socket.id) {
+			matchmaking = [];
+		}
+		//Removes user from current game and notifices other player - could use the game-id from player object but cbs
+		for (key in games) {
+			let game = games[key];
+			if (game.player1 == socket.id) {
+				users[game.player2].socket.emit('player-left');
+				delete games[key];
+			} else if (game.player2 == socket.id) {
+				users[game.player1].socket.emit('player-left');
+				delete games[key];
+			}
+		}
+	});
+});
+
+//Very simple matchmaking, as soon as theres two people in queue it matches them together
+//theoretically there should never be more than two people in the queue at any one time.
+function matchMaker(new_player) {
+	if (matchmaking.length != 0) {
+		var game = new Game(
+			matchmaking[0],
+			users[matchmaking[0]].username,
+			new_player,
+			users[new_player].username
+		);
+		games[game.id] = game;
+
+		//This is all completely un-needed but may be useful for future additions to the game
+		users[matchmaking[0]].game.id = game.id;
+		users[new_player].game.id = game.id;
+		users[matchmaking[0]].game.playing = true;
+		users[new_player].game.playing = true;
+
+		//Tells players that a game has started - allows client to initialise view
+		users[matchmaking[0]].socket.emit('game-started', {
+			username: users[matchmaking[0]].username,
+			player: 1,
+			opp_username: users[new_player].username,
+			ball: game.ball
+		});
+		users[new_player].socket.emit('game-started', {
+			username: users[new_player].username,
+			player: 2,
+			opp_username: users[matchmaking[0]].username
+		});
+		console.log(`Game ${game.id} has started.`);
+		matchmaking = [];
+	} else {
+		matchmaking.push(new_player);
+	}
+}
+
+//Sends new game data to each of the respective players in each game, every x milliseconds
+setInterval(() => {
+	for (key in games) {
+		let game = games[key];
+		if (game.state == 2) continue;
+		console.log(JSON.stringify(game));
+
+		game.update();
+		data = {
+			1: {
+				score: game.players[game.player1].score,
+				pos: game.players[game.player1].pos
+			},
+			2: {
+				score: game.players[game.player2].score,
+				pos: game.players[game.player2].pos
+			},
+			ball: game.ball
+		};
+
+		users[game.player2].socket.emit(
+			'game-data',
+			{
+				score: data[2].score,
+				opp_score: data[1].score,
+				opp_pos: data[1].pos,
+				ball: data.ball,
+				game_state: game.state,
+				winner: game.winner
+			},
+			callback => {
+				game.players[game.player2].pos = callback;
+			}
+		);
+		users[game.player1].socket.emit(
+			'game-data',
+			{
+				score: data[1].score,
+				opp_score: data[2].score,
+				opp_pos: data[2].pos,
+				ball: data.ball,
+				game_state: game.state,
+				winner: game.winner
+			},
+			callback => {
+				game.players[game.player1].pos = callback;
+			}
+		);
+
+		// if (game.state == 2) {
+		// 	users[game.player2].socket.emit('palyer-win', {
+		// 		winner: game.winner
+		// 	});
+		// 	users[game.player1].socket.emit('palyer-win', {
+		// 		winner: game.winner
+		// 	});
+		// }
+	}
+}, (1 / HERTZ) * 1000);
---
+++ public/client.js
@@ -0,0 +1,231 @@
+var host = window.location.href;
+console.log(host);
+var socket = io.connect('localhost');
+
+let game_state;
+
+//Changes text on searching for match page
+let i = '';
+let interval = setInterval(() => {
+	document.getElementById('searching-for-match').innerHTML =
+		'Searching for Match' + i;
+	i += '.';
+	if (i == '.....') i = '';
+}, 500);
+
+//Control Ping
+let ping_interval = setInterval(() => {
+	// let time = Date.now();
+	// socket.emit('get-ping', callback => {
+	// 	document.getElementById('ping').innerHTML = `Ping: ${Date.now() -
+	// 		time}ms`;
+	// });
+}, 500);
+
+//Gets number of online players
+socket.on('player-broadcast', players => {
+	document.getElementById('online-players').innerHTML = `Online: ${players}`;
+});
+
+//Game has begun
+socket.on('game-started', data => {
+	clearInterval(interval);
+	game_state = new Pong(
+		data.username,
+		data.player,
+		data.opp_username,
+		data.ball
+	);
+	interval = setInterval(() => {
+		game_state.update();
+	}, (1 / 60) * 1000);
+	document.getElementById('match-making').remove();
+	document.getElementById('gameplay').style.display = 'flex';
+	fit_canvas();
+});
+
+//Gets new game data and mutates gamestate
+socket.on('game-data', (data, callback) => {
+	game_state.game.self.score = data.score;
+	game_state.game.opp.score = data.opp_score;
+	game_state.game.ball = data.ball;
+	game_state.game.opp.pos = data.opp_pos;
+	callback(game_state.game.self.pos);
+
+	if (data.game_state == 2) {
+		setTimeout(() => {
+			document.getElementById('gameplay').style.display = 'none';
+			alert(`Player ${data.winner} win!`)
+		}, 3000);
+	}
+});
+
+//Makes matchmaking div visible
+socket.on('matchmaking-begin', () => {
+	document.getElementById('match-making').style.display = 'block';
+});
+
+//Fit canvas to screen on resize
+window.addEventListener('resize', fit_canvas);
+function fit_canvas() {
+	let canvas = document.getElementById('drawing-canvas');
+	let parent = document.getElementById('gameplay');
+	canvas.height = parent.offsetHeight - 10;
+	canvas.width = parent.offsetWidth - 10;
+}
+
+//Sends username to server
+function setUsername() {
+	socket.emit(
+		'set-username',
+		document.getElementById('input-username').value,
+		callback => {
+			if (callback) {
+				document.getElementById('start-screen').remove();
+				console.log('username changed successfully');
+			} else {
+				window.alert(
+					'Username invalid, must be more than 3 characters in length, no spaces and unique. (^[a-zA-Z0-9_.-]{3,}$)'
+				);
+			}
+		}
+	);
+}
+
+//Single Player vs CPU
+function singlePlayer() {
+	//Controls
+	//Keyboard
+	let KEYMAP = {};
+	KEYMAP[87] = false;
+	KEYMAP[83] = false;
+	KEYMAP[38] = false;
+	KEYMAP[40] = false;
+	document.addEventListener('keydown', function (event) {
+		KEYMAP[event.keyCode] = true;
+	});
+	document.addEventListener('keyup', function (event) {
+		KEYMAP[event.keyCode] = false;
+	});
+	game = new Game(
+		0,
+		2,
+		1,
+		4
+	)
+	game_state = new Pong(
+		"Player One",
+		1,
+		"Player Two",
+		[50, 50]
+	);
+	clearInterval(interval);
+	interval = setInterval(() => {
+		if (KEYMAP[87] || KEYMAP[38]) game_state.upSelf();
+		if (KEYMAP[83] || KEYMAP[40]) game_state.downSelf();
+		game_state.update();
+		game.update();
+		game_state.game.self.score = game.players[0].score;
+		game.players[0].pos = game_state.game.self.pos
+		game_state.game.opp.score = game.players[1].score;
+		game.players[1].pos = game_state.game.opp.pos
+		game_state.game.ball = game.ball;
+		if (game_state.game.opp.pos < game_state.game.ball[1]) game_state.game.opp.pos += 0.65;
+		if (game_state.game.opp.pos > game_state.game.ball[1]) game_state.game.opp.pos -= 0.65;
+	}, (1 / 60) * 1000);
+
+	document.getElementById('match-making').remove();
+	document.getElementById('gameplay').style.display = 'flex';
+	fit_canvas();
+}
+
+//Two Player 1v1
+function oneVerseOne() {
+	//Controls
+	//Keyboard
+	let KEYMAP = {};
+	KEYMAP[87] = false;
+	KEYMAP[83] = false;
+	KEYMAP[38] = false;
+	KEYMAP[40] = false;
+	document.addEventListener('keydown', function (event) {
+		KEYMAP[event.keyCode] = true;
+	});
+	document.addEventListener('keyup', function (event) {
+		KEYMAP[event.keyCode] = false;
+	});
+	game = new Game(
+		0,
+		2,
+		1,
+		4
+	)
+	game_state = new Pong(
+		"Player One",
+		1,
+		"Player Two",
+		[50, 50]
+	);
+	clearInterval(interval);
+	interval = setInterval(() => {
+		if (KEYMAP[87]) game_state.upSelf();
+		if (KEYMAP[83]) game_state.downSelf();
+		if (KEYMAP[38]) game_state.upOpp();
+		if (KEYMAP[40]) game_state.downOpp();
+		game_state.update();
+		game.update();
+		game_state.game.self.score = game.players[0].score;
+		game.players[0].pos = game_state.game.self.pos
+		game_state.game.opp.score = game.players[1].score;
+		game.players[1].pos = game_state.game.opp.pos
+		game_state.game.ball = game.ball;
+	}, (1 / 60) * 1000);
+
+	document.getElementById('match-making').remove();
+	document.getElementById('gameplay').style.display = 'flex';
+	fit_canvas();
+}
+
+//Handles opponent leaving game
+socket.on('player-left', () => {
+	socket.disconnect();
+	document.location.reload();
+});
+
+//Mouse
+$('#drawing-canvas').mousemove(function (e) {
+	let mouse_pos = getMousePos(e);
+	game_state.game.self.pos =
+		(mouse_pos.y / document.getElementById('drawing-canvas').height) * 100;
+});
+
+function getMousePos(evt) {
+	let canvas = document.getElementById('drawing-canvas');
+	var rect = canvas.getBoundingClientRect();
+	return {
+		x: evt.clientX - rect.left,
+		y: evt.clientY - rect.top
+	};
+}
+
+//Mobile
+document.addEventListener('touchstart', touchHandler);
+document.addEventListener('touchmove', touchHandler);
+
+function touchHandler(e) {
+	if (e.touches) {
+		let playerY =
+			e.touches[0].pageY -
+			document.getElementById('drawing-canvas').offsetTop;
+		game_state.game.self.pos =
+			(playerY / document.getElementById('drawing-canvas').height) * 100;
+		e.preventDefault();
+	}
+}
+
+document.getElementById('input-search-user').addEventListener('change', (data) => {
+	socket.emit('search-users', data.target.value);
+});
+socket.on('search-users-result', result => {
+	console.log(result);
+})
